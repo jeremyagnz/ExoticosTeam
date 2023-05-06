ddfdfbc30bc882b08661bc95270203c8cdb54279
jeremyagnz <jeremyagnz@gmail.com>
2023-05-06 19:09:33 -0400

    Updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NewsComponent } from './news/news.component';
+import { AsidebarComponent } from './asidebar/asidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NewsComponent,
+    AsidebarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/asidebar/asidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asidebar',
+  templateUrl: './asidebar.component.html',
+  styleUrls: ['./asidebar.component.css']
+})
+export class AsidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
